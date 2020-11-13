f8dd59bd31021ccb8416545dd30c0474d387f493
Moreen <moreenkim36@gmail.com>
2020-11-13 12:14:12 +0300

    Add a route for the allbootcamps page

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import './App.css';
 
 import BootcampDetail from './container/BootcampDetail';
+import AllBootcamps from './container/AllBootcamps';
 
 function App() {
   return (
     <div>
       <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">BootCamps</NavbarBrand>
+        <NavbarBrand href="/">DevCamper</NavbarBrand>
         <Nav className="mr-auto" navbar>
           <NavItem>
             <NavLink href="/bootcamps">BootcampDetail</NavLink>
+            <NavLink href="/">Bootcamps</NavLink>
           </NavItem>
         </Nav>
       </Navbar>
 
       <Switch>
         <Route exact path="/bootcamps" component={BootcampDetail} />
+        <Route exact path="/" component={AllBootcamps} />
       </Switch>
     </div>
   );
