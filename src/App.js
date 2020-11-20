1324d7a6ecb7da234457b108b758a1c5cdee81de
Moreen <moreenkim36@gmail.com>
2020-11-20 10:07:14 +0300

    Fix several bugs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import './App.css';
 
 import BootcampDetail from './container/BootcampDetail';
 import AllBootcamps from './container/AllBootcamps';
-import DetailViewCard from './component/DetailViewCard';
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
         <NavbarBrand href="/">DevCamper</NavbarBrand>
         <Nav className="mr-auto" navbar>
           <NavItem>
-            {/* <NavLink href="/bootcamps">BootcampDetail</NavLink> */}
             <NavLink href="/">Bootcamps</NavLink>
           </NavItem>
         </Nav>
